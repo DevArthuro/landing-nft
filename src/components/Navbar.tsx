7c2a6d96d26f488bb8e3807277bd848917dbbd8f
Carlos Orrego <carlosorrego5151@gmail.com>
2024-08-06 23:33:14 -0500

    feat: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import Logo from "../../public/logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [navState, setNavState] = useState(false);
+  return (
+    <nav>
+      <div className="brand-container">
+        <div className="brand">
+          <img src={Logo} alt="logo" className="logo" />
+        </div>
+        <div className="toggle-container">
+          <div className="toggle"></div>
+          <div className="mode"></div>
+        </div>
+      </div>
+      <div className="links-container">
+        <ul className="links">
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Launch</a>
+          </li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
