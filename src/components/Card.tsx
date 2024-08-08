91c10e7290ab81188f8a228b41572bbcb08074dc
Carlos Orrego <carlosorrego5151@gmail.com>
2024-08-08 17:14:31 -0500

    feat(components): create card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,39 @@
-const Card = () => {
-  return <div>Card</div>;
+import EthLogo from "../../public/supereth.png";
+
+interface Props {
+  image: string;
+  series: string;
+  title: string;
+  tag: number;
+  price: number;
+  time: number;
+}
+
+const Card: React.FC<Props> = ({ image, price, series, tag, time, title }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={image} alt="image card  " />
+      </div>
+      <div className="card-content">
+        <div className="card-heading">
+          <span className="card-series">{series}</span>
+          <span className="card-top">Top bid</span>
+        </div>
+        <div className="card-details">
+          <h4 className="card-title">{title}</h4>
+          <div className="card-price">
+            <img src={EthLogo} alt="Eth logo" />
+            <h4>{price} ETH</h4>
+          </div>
+        </div>
+        <div className="card-sub-details">
+          <span>#{tag}</span>
+          <span>{time} day left</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
