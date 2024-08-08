86c237679f18727c61f86e42414ea15238d566cf
Carlos Orrego <carlosorrego5151@gmail.com>
2024-08-07 22:08:13 -0500

    feat: add the home section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,30 @@
+import HomeLogo from "../../public/home.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="content">
+          <p className="sub-title">Launching soon</p>
+          <h1 className="title">An NTF like no another</h1>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up below to
+            recieve updates when we go live
+          </p>
+          <button>Sign up</button>
+        </div>
+        <div className="image-container">
+          <div className="image">
+            <img src={HomeLogo} alt="home logo" />
+          </div>
+          <div className="ellipse-container">
+            <div className="ellipse pink"></div>
+            <div className="ellipse orange"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
