6ae692f46bffad9aea1815b0d2182c64904d2829
Carlos Orrego <carlosorrego5151@gmail.com>
2024-08-11 23:52:30 -0500

    feat: add like section

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,5 +1,43 @@
+import eth1 from "../../public/eth1.png";
+import eth2 from "../../public/eth2.png";
+
 const Like = () => {
-  return <div>Like</div>;
+  return (
+    <div className="like">
+      <div className="container">
+        <div className="content">
+          <div className="image">
+            <img src={eth1} alt="Eth logo" />
+          </div>
+          <h2 className="title">An NFT like no other</h2>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up bellow to
+            recieve updates when we go live on 11/24
+          </p>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up bellow to
+            recieve updates when we go live on 11/24. Don't miss out on the
+            release of our new NFT.
+          </p>
+        </div>
+        <div className="content">
+          <div className="image">
+            <img src={eth2} alt="Eth logo" />
+          </div>
+          <h2 className="title">An NFT like no other</h2>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up bellow to
+            recieve updates when we go live on 11/24
+          </p>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up bellow to
+            recieve updates when we go live on 11/24. Don't miss out on the
+            release of our new NFT.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Like;
